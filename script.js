bcd53200d67a98697f12ab8a07f4fda7ee76d205
Krrish105 <jainkarishma76@gmail.com>
2022-02-05 18:05:16 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-item");
 
 hamburger.addEventListener("click", function(){
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 })
-
-navLink.forEach((item) => {
-     item.addEventListener("click", function(){
-       hamburger.classList.remove("active");
-       navMenu.classList.remove("active");
-     })
-});
